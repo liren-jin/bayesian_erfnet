74d70866b5442b9ee87935386891894494809d17
Liren Jin <ljin@uni-bonn.de>
2023-08-19 16:15:02 +0200

    add import path

--- trained_model.py
+++ trained_model.py
@@ -1,6 +1,11 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+import torch
 from models.modules import ERFNetModel
 from models.modules import AleatoricERFNetModel
-import torch
 from utils.utils import *
 
 
